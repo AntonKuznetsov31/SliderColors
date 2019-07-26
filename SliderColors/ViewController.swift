c5ffdfd2456e95ee29a9bda987cf3d673aa31474
Anton Kuznetsov <anton.kuznetsov.mail@gmail.com>
2019-07-26 18:00:47 +0300

    1. Constraints created, 1 warning

--- SliderColors/ViewController.swift
+++ SliderColors/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
 }
-
